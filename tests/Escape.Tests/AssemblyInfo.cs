849f3f81c62069b0bd2efe03d5e80a0366314bef
Atif Aziz <code@raboof.com>
2014-07-18 01:06:59 +0200

    Test project assembly info cleanup

---
+++ tests/Escape.Tests/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Escape.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Escape.Tests")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2013, Sebastien Ros. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83700b70-9c25-4f06-8af1-623b22b865d0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
