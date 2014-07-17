bf2e23b40c2e195e28b747c1d9361251b8e3b4e5
Atif Aziz <code@raboof.com>
2014-07-17 17:50:35 +0200

    License headers and copyright fixes

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,3 +1,39 @@
+#region BSD 2-Clause License
+//
+// Copyright (c) 2014, Atif Aziz
+// Copyright (c) 2013, Sebastien Ros
+// Copyright (C) 2013, Thaddee Tyl <thaddee.tyl@gmail.com>
+// Copyright (C) 2012, Mathias Bynens <mathias@qiwi.be>
+// Copyright (C) 2012, Joost-Wim Boekesteijn <joost-wim@boekesteijn.nl>
+// Copyright (C) 2012, Kris Kowal <kris.kowal@cixar.com>
+// Copyright (C) 2012, Yusuke Suzuki <utatane.tea@gmail.com>
+// Copyright (C) 2012, Arpad Borsos <arpad.borsos@googlemail.com>
+// Copyright (C) 2011, Ariya Hidayat <ariya.hidayat@gmail.com>
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#endregion
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -10,7 +46,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Escape")]
-[assembly: AssemblyCopyright("Copyright \u0021 2014")]
+[assembly: AssemblyCopyright(
+    "Copyright \u00a9 2014, Atif Aziz"
+          + ", \u00a9 2013, Sebastien Ros"
+          + ", \u00a9 2013, Thaddee Tyl"
+          + ", \u00a9 2012, Mathias Bynens"
+          + ", \u00a9 2012, Joost-Wim Boekesteijn"
+          + ", \u00a9 2012, Kris Kowal"
+          + ", \u00a9 2012, Yusuke Suzuki"
+          + ", \u00a9 2012, Arpad Borsos"
+          + ", \u00a9 2011, Ariya Hidayat")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
